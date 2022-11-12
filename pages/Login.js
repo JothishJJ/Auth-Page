298795fe3d055c8a7f3a850ccbaa3517ba645e87
Jothish JJ <jothishjj11@gmail.com>
2022-11-12 16:25:49 +0000

    Setup Log In page

--- pages/Login.js
+++ pages/Login.js
@@ -6,12 +6,11 @@ import { useRef, useState } from "react";
 
 import { useAuth } from "../lib/AuthContext";
 
-export default function Signup() {
+export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
 
-  const { signup } = useAuth();
+  const { login } = useAuth();
 
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -19,15 +18,11 @@ export default function Signup() {
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
-
     try {
       setError("");
-      await signup(emailRef.current.value, passwordRef.current.value);
+      await login(emailRef.current.value, passwordRef.current.value);
     } catch {
-      setError("Failed to create an account");
+      setError("Failed to sign in");
     }
 
     setLoading(false);
@@ -45,7 +40,7 @@ export default function Signup() {
         <div className="w-100" style={{ maxWidth: "400px" }}>
           <Card>
             <Card.Body>
-              <h2 className="text-center md-4">Sign Up</h2>
+              <h2 className="text-center md-4">Log In</h2>
               <Form onSubmit={handleSubmit}>
                 <Form.Group id="email">
                   <Form.Label>Email</Form.Label>
@@ -57,29 +52,20 @@ export default function Signup() {
                   <Form.Control type="password" ref={passwordRef} required />
                 </Form.Group>
                 <br />
-                <Form.Group id="password-confirm">
-                  <Form.Label>Password Confirmation</Form.Label>
-                  <Form.Control
-                    type="password"
-                    ref={passwordConfirmRef}
-                    required
-                  />
-                </Form.Group>
-                <br />
                 <Button
                   disabled={loading}
                   type="submit"
                   className="w-100"
                   variant="success"
                 >
-                  Sign Up
+                  Log In
                 </Button>
               </Form>
               {error && <Alert variant="danger">{error}</Alert>}
             </Card.Body>
           </Card>
           <div className="w-100 text-center mt-2">
-            Already have an account? <Link href="/Login">Log In</Link>
+            Don't have an account? <Link href="/Signup">Sign Up</Link>
           </div>
         </div>
       </Container>
